1d36ccf975f10311d2e1eaa6a6c02603263dc0a1
roroyu <yuroro@msn.com>
2020-11-17 06:44:28 +0000

    Isolate data and programs

--- data/_db.js
+++ data/_db.js
@@ -2,19 +2,21 @@ const load = require("json-server/lib/cli/utils/load")
 
 const Path = require("path");
 const glob = require("glob");
-const apiFiles = glob.sync(Path.resolve(__dirname, "./") + "/**/[!_]*.json", {
+// const apiFiles = glob.sync(Path.resolve(__dirname, "./") + "/**/[!_]*.json", {
+const apiFiles = glob.sync(Path.resolve(__dirname, "../../json-data") + "/**/[!_]*.json", {
   nodir: true
 });
 let data = {};
 
 var promises = [];
 apiFiles.forEach(filePath => {
-    let [, url] = filePath.split("data/");
+    let [, url] = filePath.split("/data/");
     url =
         url.slice(url.length - 9) === "/index.js"
         ? url.slice(0, url.length - 9) // remove /index.js
         : url.slice(0, url.length - 5); // remove .js
     // const api = require(filePath);
+    console.log('url: ', url)
     console.log('filePath: ', filePath)
     promises.push(load(filePath).then(res => {
         console.log('res: ', res.value().length)
